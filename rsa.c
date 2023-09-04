e88559673311c01f14511ee272b4a7563096e772
HazemUsama <hazemosama681@gmail.com>
2023-09-04 23:35:22 +0300

    hmmm

--- rsa.c
+++ rsa.c
@@ -15,20 +15,18 @@ char *readLine(FILE *file)
 	}
 	return line;
 }
-char *f(mpz_t x, ll c, mpz_t mod)
+void f(mpz_t x, ll c, mpz_t mod)
 {
 	mpz_mul(x, x, x);
 	mpz_add_ui(x, x, c);
 	mpz_mod(x, x, mod);
-	return mpz_get_str(NULL, 10, x);
 }
 
 void rho(char *num, ll x0, ll c)
 {
 	
 	mpz_t x, g, p, xs, y, n, sub;
-	char *ans = NULL;
-	
+
 	mpz_init_set_ui(x, x0);
 	mpz_init_set_ui(g, 1);
 	mpz_init_set_ui(p, 1);
@@ -42,14 +40,14 @@ void rho(char *num, ll x0, ll c)
 	{
 		mpz_set(y, x);
 		for (ll i = 1; i < l; i++)
-			mpz_set_str(x, f(x, c, n), 10);
+			f(x, c, n);
 		ll k = 0;
 		while (k < l && mpz_cmp_ui(g, 1) == 0)
 		{
 			mpz_set(xs, x);
 			for (ll i = 0; i < m && i < l - k; i++)
 			{
-				mpz_set_str(x, f(x, c, n), 10);
+				f(x, c, n);
 				mpz_sub(sub, x, y);
 				mpz_abs(sub, sub);
 				mpz_mul(p, p, sub);
@@ -62,7 +60,7 @@ void rho(char *num, ll x0, ll c)
 		if (mpz_cmp(g, n) == 0)
 		{
 			do {
-				mpz_set_str(xs, f(xs, c, n), 10);
+				f(xs, c, n);
 				mpz_sub(sub, xs, y);
 				mpz_abs(sub, sub);
 				mpz_gcd(g, sub, n);
@@ -76,13 +74,14 @@ void rho(char *num, ll x0, ll c)
 		}
 	}
 
-	ans = mpz_get_str(NULL, 10, g);
 	mpz_out_str(stdout, 10, n);
-	printf("=%s*", ans);
+	printf("=");
+	mpz_out_str(stdout, 10, g);
+	printf("*");
 	mpz_divexact(n, n, g);
 	mpz_out_str(stdout, 10, n);
 	printf("\n");
-	free(ans);
+
 	mpz_clear(x); mpz_clear(g); mpz_clear(p); mpz_clear(xs);
 	mpz_clear(y); mpz_clear(n); mpz_clear(sub);
 }
